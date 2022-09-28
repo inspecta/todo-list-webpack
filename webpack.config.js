a3a0ed76d932bfccd9963fc7be8cb3561597f486
derrick <djmulinde17@gmail.com>
2022-09-28 16:24:37 +0300

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].js',
+    filename: 'main.js',
     path: path.resolve(__dirname, './dist'),
     clean: true,
   },
