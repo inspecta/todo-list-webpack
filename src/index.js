52170ea4d59c667afe1cef7bfcc21e171fe5f35a
derrick <djmulinde17@gmail.com>
2022-09-28 14:55:45 +0300

    Add html and css source directory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
-import _ from "lodash";
 import './style.css';
 
-const content = document.createElement('div');
-const title = document.createElement('h1');
-title.innerHTML = 'Hello Webpack!';
+const todoListContainer = document.querySelector('.todo-list');
 
-document.body.appendChild(content);
-content.appendChild(title);
+const horizontalLine = (parent) => {
+  const hr = document.createElement('hr');
+  parent.appendChild(hr);
+};
+
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Go to market',
+    completed: true,
+    index: 3,
+  },
+];
+
+const title = document.createElement('h2');
+title.innerHTML = 'Todays To Do';
+todoListContainer.appendChild(title);
+horizontalLine(title);
+
+const input = document.createElement('div');
+input.classList.add('add-task');
+todoListContainer.appendChild(input);
+input.innerHTML = '<input type=\'text\' placeholder=\'Add to your list...\' class=\'input-task\'/>';
+
+const tasksLists = document.createElement('ul');
+tasksLists.classList.add('tasks-list');
+todoListContainer.appendChild(tasksLists);
+
+tasks.forEach((task) => {
+  const taskDetails = document.createElement('li');
+  tasksLists.appendChild(taskDetails);
+  taskDetails.innerHTML = `
+    <hr/>
+    <div class='task'>
+      <input type='checkbox' class='checkbox' value=''/>
+      <p>${task.description}</p>
+    </div>
+  `;
+});
+
+const lowDiv = document.createElement('div');
+lowDiv.classList.add('footer');
+todoListContainer.appendChild(lowDiv);
+lowDiv.innerHTML = `
+  <hr/>
+  <p>Clear all completed.</p>
+`;
