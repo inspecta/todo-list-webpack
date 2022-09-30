6b11ea4d740a4e20e04d83aa49301241096868e7
derrick <djmulinde17@gmail.com>
2022-09-30 10:59:45 +0300

    Update main js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './style.css';
-import updateTasks from './modules/updateTasks.js';
 import retrieveFromStorage from './modules/retrieveFromStorage.js';
 import displayTasks, { footer } from './modules/displayTasks.js';
 import addTask from './modules/addTask.js';
-import removeTask from './modules/removeTask.js';
 
 const totalTasks = JSON.parse(retrieveFromStorage('todo')) || [];
-
-displayTasks(totalTasks);
-footer();
-
 const tasksListDisplay = document.querySelector('.display');
 
 if (totalTasks.length === 0) {
@@ -19,8 +13,9 @@ if (totalTasks.length === 0) {
 /* Add a new task */
 addTask();
 
-/* Remove a task */
-removeTask(totalTasks);
-
-/* Edit a task */
-updateTasks(totalTasks);
+window.addEventListener('DOMContentLoaded', () => {
+  totalTasks.forEach((i) => {
+    displayTasks(i);
+  });
+  footer();
+});
